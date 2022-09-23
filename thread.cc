c3ed61af07292467da0bac76ec9df46b8c728f5d
Mariana <dickmariana1@gmail.com>
2022-09-22 23:57:43 -0300

    implement thread  dispatcher

--- thread.cc
+++ thread.cc
@@ -14,7 +14,7 @@ int Thread::switch_context(Thread * prev, Thread * next)
     // Verifica os id's das 2 para ver se não são a mesma thread
     if (prev->id() != next->id()) {
         Thread::_running = next;
-        return CPU::switch_context(prev->context(), next->context());
+        return CPU::switch_context(prev->_context(), next->_context());
     }
     return 0;
 };
@@ -31,6 +31,36 @@ void Thread::thread_exit (int exit_code)
 int Thread::id() 
 {
     return _id;
+};
+
+static void Thread::dispatcher()
+{
+    db<Thread>(TRC) << ">> Scheduling next thread.";
+    while (!Thread::_ready.empty()) {
+        // Escolhe primeira thread da fila para ser executada
+        Thread * next = Thread::_ready.head();
+        // Adiciona dispatcher na fila novamente
+        Thread::_dispatcher._state = READY;
+        Thread::_ready.insert_tail(&Thread::_dispatcher._link);
+        // Atualiza ponteiro _running
+        Thread::_running = next;
+        // Atualiza o state da próxima thread
+        next._state = RUNNING;
+        // Troca o contexto
+        Thread::switch_context(Thread::_dispatcher, Thread::_running);
+        if (Thread::_running._state == FINISHING) {
+            Thread::_ready.remove_head();
+            Thread::_thread_counter--;
+        }
+    }
+    Thread::_dispatcher._state = FINISHING;
+    Thread::_ready.remove_tail();
+    Thread::switch_context(Thread::_dispatcher, Thread::_main);
+};
+
+static void Thread::init(void (*main)(void *))
+{
+    db<Thread>(TRC) << ">> Threads main and dispatcher are being initialized.\n";
 };
 
 CPU::Context * Thread::context()
