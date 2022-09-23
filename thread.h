6de2a8e958eb4ec8f4870b34e14138b7138b907a
Mariana <dickmariana1@gmail.com>
2022-09-22 21:17:37 -0300

    changing thread header file to fit new implementations

--- thread.h
+++ thread.h
@@ -4,6 +4,9 @@
 #include "cpu.h"
 #include "traits.h"
 #include "debug.h"
+#include "list.h"
+#include <ctime> 
+#include <chrono>
 
 __BEGIN_API
 
@@ -11,8 +14,22 @@ class Thread
 {
 protected:
     typedef CPU::Context Context;
-
 public:
+
+    typedef Ordered_List<Thread> Ready_Queue;
+
+    // Thread State
+    enum State {
+        RUNNING,
+        READY,
+        FINISHING
+    };
+
+    /*
+     * Construtor vazio. Necessário para inicialização, mas sem importância para a execução das Threads.
+     */ 
+    Thread() { }
+
     /*
      * Cria uma Thread passando um ponteiro para a função a ser executada
      * e os parâmetros passados para a função, que podem variar.
@@ -48,29 +65,53 @@ public:
     int id();
 
     /*
-     * Qualquer outro método que você achar necessário para a solução.
+     * NOVO MÉTODO DESTE TRABALHO.
+     * Daspachante (disptacher) de threads. 
+     * Executa enquanto houverem threads do usuário.
+     * Chama o escalonador para definir a próxima tarefa a ser executada.
      */
+    static void dispatcher(); 
 
     /*
-     * Retorna o contexto
-     */
-    Context * context();
+     * NOVO MÉTODO DESTE TRABALHO.
+     * Realiza a inicialização da class Thread.
+     * Cria as Threads main e dispatcher.
+     */ 
+    static void init(void (*main)(void *));
+
 
     /*
-     * Destrutor
+     * Devolve o processador para a thread dispatcher que irá escolher outra thread pronta
+     * para ser executada.
      */
+    static void yield(); 
+
+    /*
+     * Destrutor de uma thread. Realiza todo os procedimentos para manter a consistência da classe.
+     */ 
     ~Thread();
+
+    /*
+     * Qualquer outro método que você achar necessário para a solução.
+     */ 
 
 private:
     int _id;
     Context * volatile _context;
     static Thread * _running;
+    
+    static Thread _main; 
+    static CPU::Context _main_context;
+    static Thread _dispatcher;
+    static Ready_Queue _ready;
+    Ready_Queue::Element _link;
+    volatile State _state;
 
     /*
      * Qualquer outro atributo que você achar necessário para a solução.
-     */
+     */ 
 
-    /*
+     /*
      * Atributo de classe para contagem do número de threads (inicializado como '0' em thread.cc) 
      */
     static unsigned int _thread_counter;
@@ -81,12 +122,13 @@ private:
     int _exit_code;
 };
 
-template<typename ... Tn> Thread::Thread(void (* entry)(Tn ...), Tn ... an)
+template<typename ... Tn>
+inline Thread::Thread(void (* entry)(Tn ...), Tn ... an) : /* inicialização de _link */
 {
     db<Thread>(TRC) << ">> Thread [" << _thread_counter << "] was created\n";
     _context = new CPU::Context(entry, an ...);
     _id = Thread::_thread_counter++;
-};
+}
 
 __END_API
 
