933ea76fdface6aa0877b253534f90f6d44b6b42
LucassCPS <lucas.pini@hotmail.com>
2022-12-10 20:56:28 -0300

    Add enemyPurple files

--- enemyPurple.cc
+++ enemyPurple.cc
@@ -1,1 +1,72 @@
-#include "header-files/enemy.h"
+#include "header-files/enemyPurple.h"
+
+__BEGIN_API
+
+int EnemyPurple::_SHOTS_DELAY = 50;
+
+EnemyPurple::EnemyPurple(Point point, Vector vector, std::shared_ptr<Sprite> shipSprite, std::shared_ptr<Sprite> deathSprite, PurpleEnemiesControl * controller) {
+    _point = point;
+    _vector = vector;
+    
+    _shipSprite = shipSprite;
+    _deathSprite = deathSprite;
+
+    _controller = controller;
+
+    _color = al_map_rgb(150, 0, 150);
+    _deathSpriteTimer = 5;
+
+    _shotsTimer = std::make_shared<Timer>(Configs::_fps);
+    _shotsTimer->create();
+    _shotsTimer->startTimer();
+}
+
+EnemyPurple::~EnemyPurple() {
+    if (_controller != nullptr)
+        _controller->removeShip(this);
+}
+
+void EnemyPurple::draw()
+{
+    if (isDead())
+    {
+        _deathSpriteTimer -= 1;
+        _deathSprite->draw_death_anim(_deathSpriteTimer, _point, 0);
+    }
+    else
+    {
+        _shipSprite->draw_tinted(_point, color, 0);
+    }
+}
+
+void EnemyPurple::update(double dt)
+{
+    _point = _point + _speed * dt;
+    if (_shotsTimer->getCount() > EnemyPurple::_SHOTS_DELAY + rand() % 60)
+    {
+        _canFire = true;
+        _shotsTimer->srsTimer();
+    }
+}
+
+void EnemyPurple::attack() { 
+    _canFire = false; 
+}
+
+ALLEGRO_COLOR EnemyPurple::getColor() { 
+    return _color; 
+}
+
+int EnemyPurple::getSize() { 
+    return 20; 
+}
+
+bool EnemyPurple::canFire() { 
+    return _canFire; 
+}
+
+bool EnemyPurple::isOutside() { 
+    return _point.x < -40; 
+}
+
+__END_API
--- header-files/enemyPurple.h
+++ header-files/enemyPurple.h
@@ -9,18 +9,47 @@
 #include "enemy.h"
 #include "Timer.h"
 #include "configs.h"
-#include "enemyPurpleControl.h"
 
 __BEGIN_API
 
-class EnemyPurple {
+class EnemyPurple : Enemy {
     public:
-        PurpleEnemy(Point point, Vector vector, std::shared_ptr<Sprite> shipSprite, std::shared_ptr<Sprite> deathSprite, PurpleEnemiesControl *control);
-        PurpleEnemy();
+        EnemyPurple(Point point, Vector vector, std::shared_ptr<Sprite> shipSprite, std::shared_ptr<Sprite> deathSprite, PurpleEnemiesControl *control);
+        ~EnemyPurple();
+
+        void draw();
+        void update(double dt);
+        void attack();
+
+        /* Getters */
+        ALLEGRO_COLOR getColor();
+        int getSize();
+        bool canFire();
+        bool isOutside();
+    
     protected:
     private:
-}
+        /* Checks if enemy is able to fire */
+        bool _canFire;
 
+        /* SHot delay*/
+        static int _SHOTS_DELAY;
+        std::shared_ptr<Timer> _shotsTimer;
+
+        /* Reference to its controller */
+        EnemyGroupPurple * _controller;
+
+        /* Enemy sprites */
+        std::shared_ptr<Sprite> _shipSprite;
+        std::shared_ptr<Sprite> _deathSprite;
+
+        Point _point;
+        Vector _vector;
+
+        /* Color and timer of sprite */
+        ALLEGRO_COLOR _color;
+        int _deathSpriteTimer;
+}
 
 __END_API
 
